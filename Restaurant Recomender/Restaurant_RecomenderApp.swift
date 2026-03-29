5d1bc52d43b226fe05f3afac0d2ff2ba14343c37
Jaxcho <jaxcho1118@gmail.com>
2026-03-29 14:07:04 -0700

    Initial Commit

---
+++ Restaurant Recomender/Restaurant_RecomenderApp.swift
@@ -0,0 +1,17 @@
+//
+//  Restaurant_RecomenderApp.swift
+//  Restaurant Recomender
+//
+//  Created by Jax Choi on 3/29/26.
+//
+
+import SwiftUI
+
+@main
+struct Restaurant_RecomenderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
